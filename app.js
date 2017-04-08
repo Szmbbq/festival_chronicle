465fa9e134a12912b8202dd4c5ea9f552c7f6a80
xxx <xxx@172-18-14-207.DYNAPOOL.NYU.EDU>
2017-04-08 15:05:08 -0400

    add remove

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Question = mongoose.model('Question');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 const app = express();
 
@@ -27,11 +29,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use the GoogleStrategy within Passport
+// passport.use(new GoogleStrategy({
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/google/callback"
+// }, function(accessToken, refreshToken, profile, done) {
+//     User.findOne({gooleId: profile.id}, function(err, user) {
+//         return done(err, user);
+//     });
+// }));
+
 // routes handler
 app.get('/', (req, res) => {
     Question.find({}, (err, questions) => {
         res.render('index', {questions: questions});
     });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 app.post('/createQuestion', (req, res) => {
@@ -46,6 +63,21 @@ app.post('/createQuestion', (req, res) => {
         }
         res.redirect('/');
     });
+});
+
+app.post('/deleteQuestion', (req, res) => {
+    Question.deleteOne({_id: req.body.delete}, function(err, question) {
+        if(err) {
+            res.send(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.get('/auth/google', passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/login'}), function(req, res) {
+    res.redirect('/');
 });
 
 app.listen(process.env.PORT || 3000);
