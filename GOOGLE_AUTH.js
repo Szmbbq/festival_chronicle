bc6bcf0df75ef7637302c61488810f6231e6b059
Szmbbq <zs720@nyu.edu>
2018-11-15 13:40:03 -0500

    Delete GOOGLE_AUTH.js

--- GOOGLE_AUTH.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleAuth: {
-        clientID: "775161009912-52djqj3ib3mi62vkqeecnd1qlvg57olr.apps.googleusercontent.com",
-        clientSecret: "-pK3t6h6YKaiFoqlt6Cv2G4R",
-        callbackURL: "http://localhost:3000/auth/google/callback"
-    }
-}
